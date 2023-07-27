be07cad8ab9e6fb81f47f86b6c49ed29041025f8
DAP <disap332@gmail.com>
2023-07-27 17:03:44 -0400

    removed server

--- client/src/Components/Navigation.tsx
+++ client/src/Components/Navigation.tsx
@@ -28,22 +28,28 @@ export const Navigation = () => {
         <Container style={{backgroundColor: 'teal', maxWidth: 'none'}}>
             <Row className="d-flex">
                 <Col className="d-flex justify-content-center d-lg-none">
-                    <Lottie 
-                            className='mt-3 d-lg-none'
+                        <a href={'https://github.com/DisAP332'} target="_blank">
+                            <Lottie 
+                            isClickToPauseDisabled={true}
+                            className='mt-3'
                             options={gitLottieOptions}
                             height={50}
                             width={50}
-                        />
+                            />
+                        </a>
                     <h1 className="ml-3 text-center nameFont mt-3">
                         Jordan                  
                     </h1>
                     <h1 className="text-center mt-3 ml-2 nameFont2">Bell</h1>
-                    <Lottie 
+                    <a href={'https://linkedin.com/in/jordan-bell-135205251/'} target="_blank">
+                        <Lottie 
+                        isClickToPauseDisabled={true}
                         className='mt-3'
                         options={linkedInLottieOptions}
                         height={50}
                         width={50}
-                    />
+                        />
+                    </a>
                 </Col>
                 <Col className="d-none d-lg-flex justify-content-center">
                     <h1 className="ml-3 text-center nameFont mt-3">
@@ -55,18 +61,24 @@ export const Navigation = () => {
                     <Row>
                         <Col className="d-flex justify-content-end mt-1">
                             <div className="d-flex">
-                            <Lottie 
-                                className='mt-3'
-                                options={gitLottieOptions}
-                                height={50}
-                                width={50}
-                            />
-                            <Lottie 
-                                className='mt-3'
-                                options={linkedInLottieOptions}
-                                height={50}
-                                width={50}
-                            />
+                                <a href={'https://github.com/DisAP332'} target="_blank">
+                                    <Lottie 
+                                    isClickToPauseDisabled={true}
+                                    className='mt-3'
+                                    options={gitLottieOptions}
+                                    height={50}
+                                    width={50}
+                                    />
+                                </a>
+                                <a href={'https://linkedin.com/in/jordan-bell-135205251/'} target="_blank">
+                                    <Lottie 
+                                    isClickToPauseDisabled={true}
+                                    className='mt-3'
+                                    options={linkedInLottieOptions}
+                                    height={50}
+                                    width={50}
+                                    />
+                                </a>
                             </div>
                         </Col>
                         <Col>
