edb4a10b0f2f8ac2480bb4be139f4ed1999f141f
DAP <disap332@gmail.com>
2023-07-27 18:30:48 -0400

    revised

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router"
+import { Navigation } from "./Components/Navigation"
+import { Home } from "./Pages/Home"
+
+function App() {
+
+  return (
+    <>
+      <div className="App animate__animated animate__fadeIn">
+      <div className="background">
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+      <div>
+
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
